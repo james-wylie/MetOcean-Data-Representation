590e19195b0ca9594e21d8af9da7cc137475ff66
James <james_wylie@outlook.com>
2020-02-05 13:37:22 +1300

    Basic Controller Functions add

---
+++ app/Http/Controllers/MetOceanDataController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\MetOceanData;
+
+class MetOceanDataController extends Controller
+{
+    public function index()
+    {
+    $OceanData = MetOceanData::latest()->paginate(25);
+    
+    return $OceanData;
+    }
+    
+    public function IndexByDay()
+    {
+    $particularDay = MetOceanData::where('date', $date)->paginate(25);
+    // Index by day function needs work.
+    return $particularDay;
+    }
+
+    public function show()
+    {
+        $particularHour = MetOceanData::where('id', $productId)->first();
+    }
+}
